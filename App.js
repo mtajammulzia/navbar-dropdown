d28c1c6b34527ca53d87a700265f3465a4dc4ffe
Tajammul <77620279+mtajammulzia@users.noreply.github.com>
2021-08-14 18:07:00 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Consulting from "./pages/Consulting";
+import Contactus from "./pages/Contactus";
+import Design from "./pages/Design";
+import Development from "./pages/Development";
+import Marketing from "./pages/Marketing";
+import Products from "./pages/Products";
+import Services from "./pages/Services";
+import Signup from "./pages/Signup";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home}></Route>
+          <Route path="/contactus" component={Contactus}></Route>
+          <Route path="/products" component={Products}></Route>
+          <Route path="/services" component={Services}></Route>
+          <Route path="/signup" component={Signup}></Route>
+          <Route path="/marketing" component={Marketing}></Route>
+          <Route path="/development" component={Development}></Route>
+          <Route path="/design" component={Design}></Route>
+          <Route path="/consulting" component={Consulting}></Route>
+        </Switch>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
