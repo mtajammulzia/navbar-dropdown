4dc8d538eba47ea0fd082e564cfb877a31320c0c
Tajammul <77620279+mtajammulzia@users.noreply.github.com>
2021-08-14 18:06:29 +0500

    Add files via upload

---
+++ pages/Development.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Development() {
+  return (
+    <div>
+      <h1 className="development">Development</h1>
+    </div>
+  );
+}
+
+export default Development;
---
+++ pages/Services.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Services() {
+  return (
+    <div>
+      <h1 className="services">Services</h1>
+    </div>
+  );
+}
+
+export default Services;
